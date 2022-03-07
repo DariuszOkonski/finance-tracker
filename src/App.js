49ac5dfef4d3b81f10cc82f921c32fa31acad35b
Dariusz Okonski <darek200180@gmail.com>
2022-03-07 17:16:07 +0000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Finance Tracker</h1>
+      <BrowserRouter>
+      
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
