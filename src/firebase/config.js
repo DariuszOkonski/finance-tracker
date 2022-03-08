70b266f8581ae66f8deb9591f4cde322a3d5589c
Dariusz Okonski <darek200180@gmail.com>
2022-03-08 14:56:18 +0000

    install and initialize firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ4d92I3iQOkMG34BwvpMH-pbBzLDdDwM",
+    authDomain: "mymoney-b7679.firebaseapp.com",
+    projectId: "mymoney-b7679",
+    storageBucket: "mymoney-b7679.appspot.com",
+    messagingSenderId: "838094144972",
+    appId: "1:838094144972:web:18c7505975efc7632f4969"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export {
+    projectFirestore
+}
