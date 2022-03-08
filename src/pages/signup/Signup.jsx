a52b22061e44fb5a9f5e407577e30418ffda5ad3
Dariusz Okonski <darek200180@gmail.com>
2022-03-08 15:44:18 +0000

    add useSignup to Signup component

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,15 +1,19 @@
 import styles from './Signup.module.css';
 import { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')  
   const [displayName, setDisplayName] = useState('')
+  const { signup, isPending, error } = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('handle submit')
-    console.log(email, password, displayName)
+    
+    signup(email, password, displayName)
+
+
   }
   
   return (
@@ -43,7 +47,11 @@ export default function Signup() {
           />
         </label>
 
-        <button className="btn">Signup</button>
+        
+        
+        { !isPending && <button className="btn">Signup</button> }
+        { isPending && <button className='btn' disabled>Loading...</button> }
+        { error && <p>{ error }</p> }
     </form>
   )
 }
