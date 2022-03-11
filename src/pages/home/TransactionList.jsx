17bcabfb0ac2fa913e50f5264b4d286b6a199c00
Dariusz Okonski <darek200180@gmail.com>
2022-03-11 15:18:51 +0000

    build TransactionList component

--- src/pages/home/TransactionList.jsx
+++ src/pages/home/TransactionList.jsx
@@ -2,6 +2,13 @@ import styles from './Home.module.css'
 
 export default function TransactionList({ transactions }) {
   return (
-    <div>TransactionList1111</div>
+    <ul className={styles.transactions}>
+      { transactions.map((transaction) => (
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}>${transaction.amount}</p>
+        </li>
+      )) }
+    </ul>
   )
 }
